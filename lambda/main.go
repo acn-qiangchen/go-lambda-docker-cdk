f97db31ca73cf74254f20da81875b1fbd2adf05d
Shearer <shearer@ShearernoMacBook-Pro.local>
2025-02-16 18:37:15 +0900

    action test

--- lambda/main.go
+++ lambda/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	//"fmt"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"net/http"
 )
 
 type Response struct {
@@ -13,7 +14,7 @@ type Response struct {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := Response{Message: "Hello from Go Lambda!"}
+	response := Response{Message: "Hello from Go Lambda by Action!"}
 	jsonResponse, _ := json.Marshal(response)
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
