01de5d0e19fb3a694da30985615e61738ac2a47f
Chen <qiang.chen@accenture.com>
2025-02-20 21:55:34 +0900

    docker compose command not found error

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -5,7 +5,7 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 #build docker image in parallel
-docker-compose build --parallel
+docker compose build --parallel
 
 #for IMAGE in image1 image2 image3; do
 for IMAGE in image1; do
@@ -15,4 +15,3 @@ for IMAGE in image1; do
 done
 
 echo "✅ All images pushed successfully!"
-
