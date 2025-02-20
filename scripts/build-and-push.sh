c01b96c797d169cf156ed1edf5bf8aa0d0a0ac74
Chen <qiang.chen@accenture.com>
2025-02-20 22:02:18 +0900

    specify compose file

--- scripts/build-and-push.sh
+++ scripts/build-and-push.sh
@@ -5,7 +5,7 @@ AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 #build docker image in parallel
-docker compose build --parallel
+docker compose build --parallel -f scripts/docker-compose.yml
 
 #for IMAGE in image1 image2 image3; do
 for IMAGE in image1; do
