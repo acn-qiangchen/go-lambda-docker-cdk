04e6e3bd202bd69a58f2daa08879704abf72c4c3
Shearer <shearer@ShearernoMacBook-Pro.local>
2025-02-21 17:07:09 +0900

    create multiple repo

--- cdk/bin/cdk-app.ts
+++ cdk/bin/cdk-app.ts
@@ -4,7 +4,7 @@ import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
 new EcrStack(app, 'EcrStack',{
-    repositoryPrefix: process.env.REPOSITORY_PREFIX || 'my-app', // Matches your GitHub Actions ECR_REPOSITORY_PREFIX
+    repositoryPrefix: process.env.ECR_REPOSITORY_PREFIX || 'my-app', // Matches your GitHub Actions ECR_REPOSITORY_PREFIX
     env: {
       account: process.env.CDK_DEFAULT_ACCOUNT,
       region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
