765002f41d0904a2ba1d58b0e048317178f51ce6
Shearer <shearer@ShearernoMacBook-Pro.local>
2025-02-21 16:57:49 +0900

    create multiple repo

--- cdk/bin/cdk-app.ts
+++ cdk/bin/cdk-app.ts
@@ -3,5 +3,11 @@ import { LambdaStack } from '../lib/lambda-stack';
 import { EcrStack } from '../lib/ecr-stack';
 
 const app = new cdk.App();
-new EcrStack(app, 'EcrStack');
+new EcrStack(app, 'EcrStack',{
+    repositoryPrefix: process.env.REPOSITORY_PREFIX || 'my-app', // Matches your GitHub Actions ECR_REPOSITORY_PREFIX
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
+    },
+  });
 new LambdaStack(app, 'LambdaStack');
---
+++ lambda/image2/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	//"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := Response{Message: "Hello from Go Lambda by Action-IMAGE2!"}
+	jsonResponse, _ := json.Marshal(response)
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers:    map[string]string{"Content-Type": "application/json"},
+		Body:       string(jsonResponse),
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
